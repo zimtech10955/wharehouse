b832d3693eac5d88c32ec7be5ae6904c34fc4507
zimtech <i8.qmobile@gmail.com>
2021-06-17 20:11:21 +0500

    Initial commit

---
+++ app/src/main/java/com/adan/itcenter/wharehouse/MainActivity.java
@@ -0,0 +1,14 @@
+package com.adan.itcenter.wharehouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
